ad11e576e37c5e958034a744e820ac6d94a3d94e
Harsh Ray <harsh.ray100@gmail.com>
2025-01-26 12:43:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const count_tags = document.querySelectorAll('count');
+
+const stats = [];
+
+count_tags.forEach(numbers => {
+    stats.push(numbers.innerHTML);
+    numbers.innerHTML = 0;
+});
+
+function formatNumber(num) {
+    return new Intl.NumberFormat().format(num);
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function loopWithDelay1(element, index) {
+    let j = parseInt(stats[index]);
+    for (let i = 0, x = j / 35; i <= j; i = i + x) {
+        await delay(50);
+        element.innerHTML = formatNumber(Math.floor(i));
+    }
+    element.innerHTML = formatNumber(j);
+
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+        if(entry.isIntersecting) {
+            loopWithDelay1(entry.target, index);
+            observer.unobserve(entry.target);
+        }
+    })
+},
+
+    {
+        threshold: 0.8,
+    }
+);
+
+
+
+async function loopWithDelay2(element) {
+    
+    for (let i = 1, j = parseInt(stats[3]); i <= j; i++) {
+        await delay(130);
+        element.innerHTML = formatNumber(i);
+    }
+}
+
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            loopWithDelay2(entry.target);
+            observer2.unobserve(entry.target);
+        }
+    })
+},
+
+    {
+        threshold: 0.8,
+    }
+);
+
+count_tags.forEach((numbers, index) => {
+    if(index !== 3) {
+        observer.observe(numbers);
+    }
+    else {
+        observer2.observe(numbers);
+    }
+});
